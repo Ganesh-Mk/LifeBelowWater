ac28c8abe1bb72acc2b322a8a8ca02e29ad85bfa
Anwar <anwarimambhai@gmail.com>
2024-05-08 12:07:45 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const UserModel = require("./models/UserModel");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-  .connect()
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+// mongoose
+//   .connect()
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connection error:", err);
+//   });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
